80ee3fc4b325267093e1e3261453efbf791cd8a9
slimnation23 <svetaberynda@gmail.com>
2023-12-18 19:05:56 +0100

    comments&mobileVersDone

--- script.js
+++ script.js
@@ -37,3 +37,22 @@ btnSubmit.addEventListener("click", () => {
 });
 
 
+// Menu toggle
+const menuBtn = document.querySelector(".menu-toggle");
+const navList = document.querySelector(".nav-list");
+const navLink = document.querySelectorAll(".nav-link");
+const header = document.querySelector(".header");
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  navList.classList.toggle("active");
+  header.classList.toggle("open");
+
+  navLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      navList.classList.remove("active");
+      header.classList.remove("open");
+    });
+  });
+}
