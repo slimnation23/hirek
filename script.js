1c47c14650d32adb51aec3e69ffca6719bdf0024
slimnation23 <svetaberynda@gmail.com>
2023-12-22 18:16:49 +0100

    darkMode

--- script.js
+++ script.js
@@ -69,3 +69,84 @@ phoneBtn.addEventListener("click", (e) => {
 cancelBtn.addEventListener("click", () => {
   formPopup.style.display = "none";
 });
+
+// Dark mode
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  const body = document.body;
+  const nav = document.querySelector(".nav");
+  const infoDesk = document.querySelector(".info-desk");
+  const warning = document.querySelector(".warning");
+  const popupDark = document.querySelector(".popup");
+  const orderForm = document.querySelector("#order_form");
+  const formDark = document.querySelector(".form");
+  const wheelCursor = document.querySelector(".wheel__cursor-text");
+  const links = document.querySelectorAll(".links");
+  const bgColor = document.querySelectorAll(".bg-dark");
+  const colorDark = document.querySelectorAll(".color-dark");
+
+  // Check the user's preference from localStorage
+  if (localStorage.getItem("dark-mode") === "enabled") {
+    body.classList.add("dark-mode");
+    nav.classList.add("dark-mode");
+    wheelCursor.classList.add("dark-mode");
+    infoDesk.classList.add("dark-mode");
+    popupDark.classList.add("dark-mode");
+    orderForm.classList.add("dark-mode");
+    formDark.classList.add("dark-mode");
+    warning.classList.add("dark-mode");
+    links.forEach((e) => {
+      e.classList.add("dark-mode");
+    });
+    colorDark.forEach((e) => {
+      e.classList.add("dark-mode");
+    });
+    bgColor.forEach((e) => {
+      e.classList.add("dark-mode");
+    });
+    darkModeToggle.checked = true;
+  }
+
+  // Toggle dark mode
+  darkModeToggle.addEventListener("change", function () {
+    if (darkModeToggle.checked) {
+      body.classList.add("dark-mode");
+      nav.classList.add("dark-mode");
+      wheelCursor.classList.add("dark-mode");
+      warning.classList.add("dark-mode");
+      popupDark.classList.add("dark-mode");
+      orderForm.classList.add("dark-mode");
+      formDark.classList.add("dark-mode");
+      infoDesk.classList.add("dark-mode");
+      links.forEach((e) => {
+        e.classList.add("dark-mode");
+      });
+      colorDark.forEach((e) => {
+        e.classList.add("dark-mode");
+      });
+      bgColor.forEach((e) => {
+        e.classList.add("dark-mode");
+      });
+      localStorage.setItem("dark-mode", "enabled");
+    } else {
+      body.classList.remove("dark-mode");
+      nav.classList.remove("dark-mode");
+      wheelCursor.classList.remove("dark-mode");
+      infoDesk.classList.remove("dark-mode");
+      orderForm.classList.remove("dark-mode");
+      warning.classList.remove("dark-mode");
+      formDark.classList.remove("dark-mode");
+      popupDark.classList.remove("dark-mode");
+      links.forEach((e) => {
+        e.classList.remove("dark-mode");
+      });
+      colorDark.forEach((e) => {
+        e.classList.remove("dark-mode");
+      });
+      bgColor.forEach((e) => {
+        e.classList.remove("dark-mode");
+      });
+      localStorage.setItem("dark-mode", "disabled");
+    }
+  });
+});
