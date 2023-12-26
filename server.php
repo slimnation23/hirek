9e133aea4a7de8fab1d3e6d1b4586db50221dab6
slimnation23 <svetaberynda@gmail.com>
2023-12-26 17:05:20 +0100

    fix

--- server.php
+++ server.php
@@ -15,30 +15,7 @@
       "Content-type" => "text/html; charset=utf8"
     ];
 
-    $body = '
-    <html>
-    <body>
-    <center>
-    <table border="1" cellpadding="6" sellspacing="0" width="90%" bordercolor="#DBDBDB">
-    <tr>
-    <td colspan="2" align="center" bgcolor="#E4E4E4"><b>Information</b></td>
-    </tr>';
-    $body .= '
-    <tr>
-    <td><b>Name</b></td>
-    <td>'. $name .'</td>
-    </tr>
-    <tr><td><b>Teleram</b></td>
-    <td>'. $tel .'</td>
-    </tr>
-    <tr>
-    <td><b>Name</b></td>
-    <td>'. $sendName .'</td>
-    </tr>
-    <tr><td><b>Teleram</b></td>
-    <td>'. $sendTel .'</td>
-    </tr>
-    ';
+    $body = "Name: $name<br>Telephone: $tel<br>Sender Name: $sendName<br>Sender Telephone: $sendTel";
 
     mail($to, $subj, $body, $headers);
 }
